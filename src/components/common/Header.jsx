b70f783a3afaa47267f5efb26338e0858934f0c2
rahulfordev <rahulforsms@gmail.com>
2024-02-03 16:19:49 +0600

    color and design change

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,20 +2,12 @@ import { NavLink } from "react-router-dom";
 import Container from "./Container";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
-// import { RxHamburgerMenu } from "react-icons/rx";
-// import { GoSignOut } from "react-icons/go";
-// import { RxCross2 } from "react-icons/rx";
-// import defaultUser from "../../assets/default__user.png";
-import { IoIosHelpCircleOutline } from "react-icons/io";
-
-import language from "../../assets/language.png";
 
 import "./Header.css";
 import { useContext } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const Header = () => {
-  // const [isShow, setIsShow] = useState(false);
   const { state, logout } = useContext(AuthContext);
 
   const handleLogOut = () => {
@@ -25,57 +17,24 @@ const Header = () => {
     <header className="header-section">
       <Container>
         <nav className="header__content">
-          {/* <div className="mobile__nav--bar">
-            <div className="mobile__nav--logo">
-              <Logo />
-            </div>
-            <ul className="mobile__nav--items">
-              {user?.email ? (
-                <li className="mobile__nav--user header__link">
-                  <img
-                    src={user?.photoURL ? user?.photoURL : defaultUser}
-                    alt=""
-                  />
-                  <span>{user.displayName}</span>
-                </li>
-              ) : null}
-              <li
-                className="mobile__nab--hamburger"
-                onClick={() => setIsShow(!isShow)}
-              >
-                {isShow ? <RxCross2 /> : <RxHamburgerMenu />}
-              </li>
-            </ul>
-          </div> */}
           <div className="header__logo">
             <h2>Booking.com</h2>
           </div>
           <div className="header__nav--links">
-            <ul className="header__links">
-              <NavLink className="header__link">USD</NavLink>
-
-              <NavLink className="header__link">
-                <div className="header__link--image">
-                  <img src={language} alt="" />
-                </div>
-              </NavLink>
-
-              <NavLink className="header__link">
-                <IoIosHelpCircleOutline />
-              </NavLink>
-            </ul>
-            <ul className="header__links links--button  ">
-              <NavLink className="header__link">List your property</NavLink>
-
+            <ul className="header__links links--button">
               {state.user ? (
                 <>
                   <NavLink
                     to={"/account-settings"}
-                    className="header__link--button button"
+                    className="header__link--button button-primary"
                   >
                     Account
                   </NavLink>
-                  <NavLink onClick={handleLogOut} to={"/"}>
+                  <NavLink
+                    className="header__link--button button-secondary"
+                    onClick={handleLogOut}
+                    to={"/"}
+                  >
                     LogOut
                   </NavLink>
                 </>
@@ -83,13 +42,13 @@ const Header = () => {
                 <>
                   <NavLink
                     to={"/signup"}
-                    className="header__link--button button"
+                    className="header__link--button button-primary"
                   >
                     Register
                   </NavLink>
                   <NavLink
                     to={"/login"}
-                    className="header__link--button button"
+                    className="header__link--button button-secondary"
                   >
                     Sign in
                   </NavLink>
@@ -98,64 +57,6 @@ const Header = () => {
             </ul>
           </div>
         </nav>
-
-        {/* mobile menu li start */}
-
-        {/* {isShow && (  
-          <div className="mobile__menu">
-            <ul className="mobile__menu--header__links">
-              <NavLink className="mobile__menu--header__link" to={"/"}>
-                Home
-              </NavLink>
-
-              <NavLink className="mobile__menu--header__link" to={"/jobs"}>
-                Jobs
-              </NavLink>
-
-              <NavLink className="mobile__menu--header__link" to={"/about"}>
-                About
-              </NavLink>
-
-              <NavLink className="mobile__menu--header__link" to={"/contact"}>
-                Contact
-              </NavLink>
-
-              <NavLink className="mobile__menu--header__link" to={"/favorite"}>
-                Favorite
-              </NavLink>
-            </ul>
-
-            {user ? (
-              <ul className="mobile__menu--header__links">
-                <li>
-                  <Link
-                    onClick={handleSignOut}
-                    className="mobile__menu--header__link"
-                  >
-                    Sign Out <GoSignOut />
-                  </Link>
-                </li>
-              </ul>
-            ) : (
-              <ul className="mobile__menu--header__links">
-                <li>
-                  <NavLink
-                    className="mobile__menu--header__link"
-                    to={"/signup"}
-                  >
-                    Sign Up
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className="mobile__menu--header__link" to={"/login"}>
-                    Sign In
-                  </NavLink>
-                </li>
-              </ul>
-            )}
-          </div>
-        )} */}
-        {/* mobile menu li end */}
       </Container>
     </header>
   );
