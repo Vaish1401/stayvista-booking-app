9a017993eb41256c044b3ef24441df442e49888c
rahulfordev <rahulforsms@gmail.com>
2024-02-03 20:48:46 +0600

    newsletter section style change

--- src/components/mailList/MailList.jsx
+++ src/components/mailList/MailList.jsx
@@ -4,13 +4,17 @@ import "./mailList.css";
 const MailList = () => {
   return (
     <div className="mail">
-      <h1 className="mailTitle">Save time, save money!</h1>
-      <span className="mailDesc">
-        Sign up and we'll send the best deals to you
-      </span>
-      <div className="mailInputContainer">
-        <input type="text" placeholder="Your Email" />
-        <button>Subscribe</button>
+      <div className="newsletter__container">
+        <div className="newsletter__content">
+          <h1 className="mailTitle">Save time, save money!</h1>
+          <span className="mailDesc">
+            Sign up and we'll send the best deals to you
+          </span>
+          <div className="mailInputContainer">
+            <input type="text" placeholder="Your Email" />
+            <button>Subscribe</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,9 +20,7 @@ const Home = () => {
           </div>
         </Container>
       </div>
-
       <Featured />
-
       <MailList />
     </>
   );
