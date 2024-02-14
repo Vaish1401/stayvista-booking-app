6f6ec13eeb7b85ccad2959ecf8db668421679c47
rahulfordev <rahulforsms@gmail.com>
2024-02-14 16:36:28 +0600

    search system add

---
+++ src/components/SearchItem/SearchItem.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import "./searchItem.css";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+
+const SearchItem = () => {
+  const { cardDatas } = useContext(AuthContext);
+  return (
+    <>
+      {cardDatas.map((card) => (
+        <div className="searchItem" key={card.id}>
+          <img src={card.image} alt="" className="siImg" />
+          <div className="siDesc">
+            <h1 className="siTitle">{card.title}</h1>
+            <span className="siDistance">500m from center</span>
+            <span className="siTaxiOp">Free airport taxi</span>
+            <span className="siSubtitle">
+              Studio Apartment with Air conditioning
+            </span>
+            <span className="siFeatures">
+              Entire studio • 1 bathroom • 21m² 1 full bed
+            </span>
+            <span className="siCancelOp">Free cancellation </span>
+            <span className="siCancelOpSubtitle">
+              You can cancel later, so lock in this great price today!
+            </span>
+          </div>
+          <div className="siDetails">
+            <div className="siRating">
+              <span>Excellent</span>
+              <button>8.9</button>
+            </div>
+            <div className="siDetailTexts">
+              <span className="siPrice">{card.price}</span>
+              <span className="siTaxOp">Includes taxes and fees</span>
+              <button className="siCheckButton">See availability</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SearchItem;
--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -1,13 +1,102 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
+
 import Container from "../../components/common/Container";
+
 import "./Hotels.css";
+import SearchItem from "../../components/SearchItem/SearchItem";
 const Hotels = () => {
   const location = useLocation();
-  console.log(location.state);
+
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.dateRange);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.person);
+
   return (
     <Container>
       <div className="hotels">
-        <h1>Hotels</h1>
+        <div className="listContainer">
+          <div className="listWrapper">
+            <div className="listSearch">
+              <h1 className="lsTitle">Search</h1>
+              <div className="lsItem">
+                <label>Destination</label>
+                <input placeholder={destination} type="text" />
+              </div>
+              <div className="lsItem">
+                <label>Check-in Date</label>
+                <span onClick={() => setOpenDate(!openDate)}>
+                  {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                    date[0].endDate,
+                    "MM/dd/yyyy"
+                  )}`}
+                </span>
+
+                {openDate && (
+                  <DateRange
+                    onChange={(item) => setDate([item.selection])}
+                    minDate={new Date()}
+                    ranges={date}
+                  />
+                )}
+              </div>
+              <div className="lsItem">
+                <label>Options</label>
+                <div className="lsOptions">
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Min price <small>per night</small>
+                    </span>
+                    <input type="number" className="lsOptionInput" />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Max price <small>per night</small>
+                    </span>
+                    <input type="number" className="lsOptionInput" />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Adult</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="lsOptionInput"
+                      placeholder={options.adult}
+                    />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Children</span>
+                    <input
+                      type="number"
+                      min={0}
+                      className="lsOptionInput"
+                      placeholder={options.children}
+                    />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Room</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="lsOptionInput"
+                      placeholder={options.room}
+                    />
+                  </div>
+                </div>
+              </div>
+              <button className="search-button">Search</button>
+            </div>
+            <div className="listResult">
+              <SearchItem />
+              <SearchItem />
+              <SearchItem />
+              <SearchItem />
+            </div>
+          </div>
+        </div>
       </div>
     </Container>
   );
